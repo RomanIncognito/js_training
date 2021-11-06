6eee52babf2ad3fcd513f0f57f0be7fa1f18a2f6
Roman Retsen <vatraltd@gmail.com>
2021-11-06 00:45:06 -0400

    nov_5_2021

--- test3.js
+++ test3.js
@@ -1,5 +1,26 @@
-let my_obj = {"a":[1,2,3]}
+function factor(x){
+    if (x == 0 || x == 1){
+        return BigInt(1)
+    } else {
+        let temp = BigInt(1)
+        for (let i = 2n; i <= x; i += 1n){
+            temp *= i
+        }
+        return temp
+    }
+}
 
+function getParticipants(handshakes){
+    if (handshakes == 0) {return 1}
+    let numberFarmers = 2
+    let numberHandshakes = BigInt(0)
+    while (true){
+        numberHandshakes = factor(numberFarmers) / (factor(2) * factor(numberFarmers - 2))
+        if (numberHandshakes >= handshakes){
+            return numberFarmers
+        }
+        numberFarmers += 1
+    }
+}
 
-
-console.log(my_obj["a"][0])
+console.log(getParticipants(56280))
